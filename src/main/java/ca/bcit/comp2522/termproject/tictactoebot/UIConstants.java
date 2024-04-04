3d6936f255897d6705e88c567ed942b48a3d5745
Alice Huang <alice.in.realityland@gmail.com>
2024-04-03 18:37:10 -0700

    Add constants

---
+++ src/main/java/ca/bcit/comp2522/termproject/tictactoebot/UIConstants.java
@@ -0,0 +1,8 @@
+package ca.bcit.comp2522.termproject.tictactoebot;
+
+public class UIConstants {
+    public static final int APP_WIDTH = 300;
+    public static final int APP_HEIGHT = 400;
+    public static final int INFO_DISPLAY_HEIGHT = 100;
+    public static final int TILE_BOARD_HEIGHT = 300;
+}
