2ed4d7f5e7cf2181d9d312000d84470ba179fc87
Alice Huang <alice.in.realityland@gmail.com>
2024-04-07 18:45:47 -0700

    Add constants

--- src/main/java/ca/bcit/comp2522/termproject/tictactoebot/UIConstants.java
+++ src/main/java/ca/bcit/comp2522/termproject/tictactoebot/UIConstants.java
@@ -4,5 +4,16 @@ public class UIConstants {
     public static final int APP_WIDTH = 300;
     public static final int APP_HEIGHT = 400;
     public static final int INFO_DISPLAY_HEIGHT = 100;
+    public static final int INFO_DISPLAY_TEXT_HEIGHT = 50;
     public static final int TILE_BOARD_HEIGHT = 300;
+    public static final int TILE_HEIGHT = 100;
+    public static final int FONT_SIZE = 24;
+    public static final int X_CENTER = 150;
+    public static final int BOARD_Y_POSITION = 250;
+    public static final int BOARD_DIMENSION = 3;
+    public static final int HASHCODE_CONSTANT = 31;
+    public static final int INFO_DISPLAY_LABEL_Y = -20;
+    public static final int START_BUTTON_HEIGHT = 30;
+    public static final int START_BUTTON_WIDTH = 135;
+    public static final int START_BUTTON_POSITION = 20;
 }
