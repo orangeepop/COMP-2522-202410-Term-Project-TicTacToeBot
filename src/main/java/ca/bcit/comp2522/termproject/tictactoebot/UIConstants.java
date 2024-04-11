9a470b7648ad77b46706c02beacfef4ebc1a1ae1
Alice Huang <alice.in.realityland@gmail.com>
2024-04-11 14:18:20 -0700

    Add javadocs

--- src/main/java/ca/bcit/comp2522/termproject/tictactoebot/UIConstants.java
+++ src/main/java/ca/bcit/comp2522/termproject/tictactoebot/UIConstants.java
@@ -1,21 +1,87 @@
 package ca.bcit.comp2522.termproject.tictactoebot;
 
+/**
+ * Contains all constants used to construct the user interface.
+ *
+ * @author Alice Huang
+ * @version 2024
+ */
 public final class UIConstants {
+    /**
+     * Width of the application window.
+     */
     public static final int APP_WIDTH = 300;
+
+    /**
+     * Height of the application window.
+     */
     public static final int APP_HEIGHT = 400;
+
+    /**
+     * Height of the information display area.
+     */
     public static final int INFO_DISPLAY_HEIGHT = 100;
+
+    /**
+     * Height of the text within the information display area.
+     */
     public static final int INFO_DISPLAY_TEXT_HEIGHT = 50;
+
+    /**
+     * Height of the tile board.
+     */
     public static final int TILE_BOARD_HEIGHT = 300;
+
+    /**
+     * Height of individual tiles.
+     */
     public static final int TILE_HEIGHT = 100;
+
+    /**
+     * Font size used within the application.
+     */
     public static final int FONT_SIZE = 24;
+
+    /**
+     * X coordinate for centering elements.
+     */
     public static final int X_CENTER = 150;
+
+    /**
+     * Y position of the board.
+     */
     public static final int BOARD_Y_POSITION = 250;
+
+    /**
+     * Dimension of the board (e.g., for Tic-Tac-Toe, it's 3x3).
+     */
     public static final int BOARD_DIMENSION = 3;
+
+    /**
+     * Constant used in calculating hash codes.
+     */
     public static final int HASHCODE_CONSTANT = 31;
+
+    /**
+     * Y position of the label within the information display area.
+     */
     public static final int INFO_DISPLAY_LABEL_Y = -20;
+
+    /**
+     * Height of the start button.
+     */
     public static final int START_BUTTON_HEIGHT = 30;
+
+    /**
+     * Width of the start button.
+     */
     public static final int START_BUTTON_WIDTH = 135;
+
+    /**
+     * Starting position of the start button.
+     */
     public static final int START_BUTTON_POSITION = 20;
+
     private UIConstants() {
     }
 }
